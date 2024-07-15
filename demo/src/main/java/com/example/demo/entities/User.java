48e50fb015614022b28c3fbb95f4f0e78ea2b147
Bruno Caffe <brunogkaffe@gmail.com>
2024-07-15 19:38:57 -0300

    Order, instant, ISO8601 - PART 1

--- demo/src/main/java/com/example/demo/entities/User.java
+++ demo/src/main/java/com/example/demo/entities/User.java
@@ -3,12 +3,13 @@ package com.example.demo.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name="tb_user")
 public class User implements Serializable {
-
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +18,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -70,6 +74,9 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     @Override
     public int hashCode() {
