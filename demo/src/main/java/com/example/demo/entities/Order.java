912e26184461be0d8564224146c29f742c9938a9
Bruno Caffe <brunogkaffe@gmail.com>
2024-07-19 11:39:21 -0300

    OrderItem, many-to-many association with extra attributes pt2

--- demo/src/main/java/com/example/demo/entities/Order.java
+++ demo/src/main/java/com/example/demo/entities/Order.java
@@ -2,10 +2,13 @@ package com.example.demo.entities;
 
 import com.example.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,6 +25,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
     }
@@ -65,6 +71,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
--- demo/src/main/java/com/example/demo/entities/OrderItem.java
+++ demo/src/main/java/com/example/demo/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package com.example.demo.entities;
 
 import com.example.demo.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -11,7 +12,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -27,6 +28,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder(){
         return id.getOrder();
     }
